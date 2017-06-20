26971c5a365d1edfa77f26afdc8fae91b084c4c4
Adans Bento <adansbento@gmail.com>
2017-06-19 22:38:50 -0300

    Aula 06

--- forca/src/forca/core.clj
+++ forca/src/forca/core.clj
@@ -3,40 +3,42 @@
 
 
 (def total-de-vidas 6)
-
 (defn perdeu[](print "Você perdeu!"))
-
 (defn ganhou[](print "Você ganhou!"))
-
 (defn le-letra! [] (read-line))
-
 (defn  acertou? [chute palavra] (.contains palavra chute))
 
-(defn letras-faltantes [ palavra acertos]
-  (remove (fn [letra] (contains? acertos (str letra))) palavra)
-)
+(defn letras-faltantes[palavra acertos]
+  (remove (fn [letra] (contains? acertos (str letra))) palavra))
 
-(defn acertou-a-palavra-toda? [palavra acertos]
-  (empty? (letras-faltantes palavra acertos))
-)
+(defn acertou-a-palavra-toda?[palavra acertos]
+  (empty? (letras-faltantes palavra acertos)))
+
+(defn imprime-forca [vidas palavra acertos]
+    (println "Vidas"  vidas)
+    (doseq [letra (seq palavra)]
+        (if (contains? acertos (str letra))
+          (print letra "")
+          (print "_" ""))
+      )
+      (println))
 
 
-(defn avalia-chute [chute vidas palavra acertos]
-    (if (acertou? chute palavra)
-        (jogo vidas palavra (conj acertos chute))
-        (jogo (dec vidas) palavra acertos)
-    )
-)
+(defn jogo[vidas palavra acertos]
+    (imprime-forca vidas palavra acertos)
+      (cond
+        (= vidas 0)(perdeu)
+        (acertou-a-palavra-toda? palavra acertos)(ganhou)
+        :else
+        (let [chute (le-letra!)]
+          (if (acertou? chute palavra)
+              (do
+                (println "Acertou a  letra!")
+                (recur vidas palavra (conj acertos chute)))
 
-(defn jogo [vidas palavra acertos]
-  (if (= vidas 0)
-    (perdeu)
-        (if (acertou-a-palavra-toda? palavra acertos)
-          (ganhou)
-          (avalia-chute (le-letra!) vidas palavra acertos)
-        )
-   )
- )
+              (do
+                (println "Errou a  letra!")
+                (recur (dec vidas) palavra acertos))))))
 
 
 
